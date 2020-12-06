800cc68475a7bc96f855196658b28af79a14464c
Abhijit-Bhosale <65159543+Abhijit-Bhosale@users.noreply.github.com>
2020-12-06 21:05:14 +0530

    Add files via upload

---
+++ Assignment4Collection_b.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4Collection_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee[] employees = new Employee[3];
+            
+            for(int i=0;i<employees.Length;i++)
+            {
+                Console.WriteLine("Enter name of employee");
+                string name= Console.ReadLine();
+                Console.WriteLine("enter salary  ");
+                decimal salary = Convert.ToDecimal(Console.ReadLine());
+                employees[i] = new Employee { Name = name, Salary = salary };
+            }
+            List<Employee> list = employees.ToList<Employee>();
+            foreach(Employee emp in list)
+            {
+                Console.WriteLine(emp.Name);
+                Console.WriteLine(emp.Salary);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Employee
+    {
+        public string name;
+        public decimal salary;
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public decimal Salary
+        {
+            set
+            {
+                salary = value;
+            }
+            get
+            {
+                return salary;
+            }
+        }
+    }
+}
---
+++ Assignment4Collection_c.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4Collection_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            for(int i=0;i<3;i++)
+            {
+                string Name;
+                decimal Salary;
+                Console.WriteLine("Enter name");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter salary");
+                Salary = Convert.ToDecimal(Console.ReadLine());
+                list.Add(new Employee { name = Name, salary = Salary });
+            }
+            Employee[] employees = list.ToArray();
+
+            foreach(Employee obj in employees)
+            {
+                Console.WriteLine("name {0}, salary{1}",obj.name,obj.salary);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Employee
+    {
+        public string name { get; set; }
+        public Decimal salary { get; set; }
+
+        
+    }
+}
